f4c49841ce31295711004c6ae471be43353c4348
mesye103 <mesye103@gmail.com>
2026-05-22 01:14:03 -0400

    feat: [AV-41][HU-16] exponer imagenUrl en almacen y agregar PATCH imagen

--- backend/usuarios-service/src/main/java/com/alovecino/usuarioservice/controller/AlmacenController.java
+++ backend/usuarios-service/src/main/java/com/alovecino/usuarioservice/controller/AlmacenController.java
@@ -9,11 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alovecino.usuarioservice.dto.AlmacenImagenRequest;
 import com.alovecino.usuarioservice.dto.AlmacenRequest;
 import com.alovecino.usuarioservice.dto.AlmacenResponse;
 import com.alovecino.usuarioservice.service.AlmacenService;
@@ -38,5 +41,12 @@ public class AlmacenController {
     @GetMapping("/mis-almacenes")
     public List<AlmacenResponse> listMisAlmacenes(@AuthenticationPrincipal Jwt jwt) {
         return almacenService.listAlmacenesByDueno(jwt.getSubject());
+    }
+
+    @PatchMapping("/{id}/imagen")
+    public AlmacenResponse updateImagenUrl(@AuthenticationPrincipal Jwt jwt,
+            @PathVariable Long id,
+            @Valid @RequestBody AlmacenImagenRequest request) {
+        return almacenService.updateImagenUrl(jwt.getSubject(), id, request);
     }
 }
