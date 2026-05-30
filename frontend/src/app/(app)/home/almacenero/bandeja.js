815225509463612b5c2e16b56d2ffd4bb116624a
mesye103 <mesye103@gmail.com>
2026-05-30 01:56:40 -0400

    feat: [AV-35][HU-10] correct  bandeja de consultas almacenero

---
+++ frontend/src/app/(app)/home/almacenero/bandeja.js
@@ -0,0 +1,5 @@
+import BandejaConsultasScreen from '../../../../features/almacenero/screens/BandejaConsultasScreen';
+
+export default function BandejaConsultasRoute() {
+  return <BandejaConsultasScreen />;
+}
